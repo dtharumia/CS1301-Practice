727baa02c3557bbff6d56e7f9394f678c2501362
dtharumia <senddharshan@gmail.com>
2021-08-02 23:29:47 -0400

    print, str intp, esc char, math import, calc

---
+++ app.py
@@ -0,0 +1,36 @@
+from math import *
+
+# print('dharshan')
+
+# msg = "this is a message as a variable"
+
+# print(msg)
+
+# player_one = 'dharshan'
+
+# print("The first player is", player_one)
+# print("it is good to meet you " + player_one + ". I hope you enjoy your stay")
+# print("Hello %s" % player_one)
+# print("Hi {}".format(player_one))
+# print(f"Hey {player_one}")
+
+# print("Dharshan\nTharumia")
+
+# print(player_one.upper())
+
+# name = "alexander"
+
+# print(name[::-1])
+
+# print(name.index("x"))
+
+# print(floor(43.234234234))
+
+# name = input('what is your name? ')
+
+# print(f"hello, {name}")
+
+num1 = input("Enter a number: ")
+num2 = input("Enter a number: ")
+result = float(num1)+float(num2)
+print(result)
